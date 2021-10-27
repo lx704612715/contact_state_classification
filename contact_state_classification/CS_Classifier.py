47a0051d4ee239bccc86addaa78ebccdb73e43cc
Hongyou Zhou <hongyou.chou@gmail.com>
2021-10-27 12:51:06 +0200

    Modularized the classifier.

---
+++ contact_state_classification/CS_Classifier.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import os
+import numpy as np
+from loguru import logger
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+
+
+class CSClassifier:
+    def __init__(self, experiment_dir, dataset_name):
+        self.experiment_dir = experiment_dir
+        self.dataset_name = dataset_name
+        self.dataset_path = self.experiment_dir + "/csd_result/" + dataset_name + ".pkl"
+        self.csd_dataset_plot_dir = self.experiment_dir + "/csd_result/plot/"
+        os.makedirs(self.csd_dataset_plot_dir, exist_ok=True)
+        self.csc_logger = logger
+
+        # Dataset
+        self.csd_data_df = None
+        self.csd_data_dict = None
+
+        # Classifier
+        self.lb = None
+        self.classifier = None
+
+        # Dataset information
+        self.all_classes = None
+        self.num_classes = None
+
+        # Train the classifier
+        self.load_data()
+        self.train_classifier(features="dist")
+
+        self.get_dataset_information()
+
+    def load_data(self):
+        # load data to dict, because processing of dataframe takes too much time
+        self.csd_data_df = pd.read_pickle(self.dataset_path)
+        self.csd_data_dict = self.csd_data_df.to_dict()
+
+    def get_traj_index_by_labels(self, label):
+        traj_index_dict = dict()
+        for key, value in self.csd_data_dict['label'].items():
+            if label in value:
+                traj_index_dict[key] = value
+
+        labels = list(set(traj_index_dict.values()))
+
+        return traj_index_dict, labels
+
+    def merge_feature_by_labels(self, traj_index_dict=None, feature="dist", labels=None):
+        feature_values = dict()
+        for label in labels:
+            feature_values[label] = []
+            traj_index_list = [key for key, value in traj_index_dict.items() if value == label]
+            for traj_index in traj_index_list:
+                feature_values[label].append(self.csd_data_dict[feature][traj_index])
+        return feature_values
+
+    def train_classifier(self, features="dist"):
+        self.lb = preprocessing.LabelBinarizer()
+        x = []
+        y = []
+        for index, row in self.csd_data_df.iterrows():
+            x.append(row[features])
+            # use the first two string values as label
+            y.append(row["label"][:2])
+        x = np.array(x)
+        self.lb.fit(y)
+        y = self.lb.transform(y)
+        num_labels = np.unique(y).shape[0] + 1
+        self.classifier = KNeighborsClassifier(num_labels).fit(x, y)
+
+    def get_dataset_information(self):
+        self.all_classes = self.lb.classes_
+        self.num_classes = len(self.all_classes)
+        self.csc_logger.info("All classes from the dataset {} are {}: ", self.dataset_name, self.all_classes)
+
+    def predict(self, input_data):
+        result = self.classifier.predict(input_data)
+        label = self.lb.inverse_transform(result)
+        return result, label
---
+++ contact_state_classification/__init__.py
@@ -0,0 +1,3 @@
+from contact_state_classification.CS_Classifier import CSClassifier
+
+__all__ = ["CS_Classifier"]
