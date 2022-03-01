81a7486716a504076b444fc61a041576df3b0faf
HongyouZhou <hongyou.chou@gmail.com>
2022-03-01 16:47:58 +0100

    Remove redundant map.

--- contact_state_classification/config.py
+++ contact_state_classification/config.py
@@ -6,23 +6,6 @@ path = {
 }
 # This is where all classifier configuration parameters are stored.
 # Since different classifiers may be used, parameters may need to be nested.
-params = {
-    "n_act": 12,
-    "use_pca": False,
-    "basic_visualization": False,
-    "circular_splicing": False,
-    "interpolation_method": "linear",
-    "upsampling_rate": 16,
-    "shaplet_size": 0.5,
-    "simple_features": ["dist"],
-    "complex_features": [],
-    "n_splits": 8,
-    "n_neighbors": 4,
-    "n_components": 3,
-    "classifier": "KNN",
-    "cs_index_map": {"CS1": 1, "CS2": 2, "CS3": 3, "CS5": 4, "CS6": 5}
-}
-
 N_ACT = 12
 USE_PCA = False
 BASIC_VISUALIZATION = True
